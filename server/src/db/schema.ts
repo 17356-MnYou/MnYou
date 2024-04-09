89770119bbace9d1c245488c167e947ef19b40c8
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2024-04-09 16:24:29 -0400

    Added relations between tables

--- server/src/db/schema.ts
+++ server/src/db/schema.ts
@@ -1,3 +1,4 @@
+import { relations } from "drizzle-orm";
 import { text } from "drizzle-orm/pg-core";
 import { boolean } from "drizzle-orm/pg-core";
 import { real } from "drizzle-orm/pg-core";
@@ -8,7 +9,15 @@ export const restaurants = pgTable("restaurants", {
   id: serial("id").primaryKey(),
   username: varchar("username", { length: 256 }).notNull().unique(),
   password: varchar("password_hash", { length: 256 }).notNull(),
+  menu: integer("menu").references(() => menus.id),
 });
+
+export const restaurantRelations = relations(restaurants, ({ one }) => ({
+  menu: one(menus, {
+    fields: [restaurants.menu],
+    references: [menus.id],
+  }),
+}));
 
 export const menus = pgTable("menus", {
   id: serial("id").primaryKey(),
@@ -22,6 +31,10 @@ export const menus = pgTable("menus", {
   address: text("address"),
 });
 
+export const menuRelations = relations(menus, ({ many }) => ({
+  menuItems: many(menuItems),
+}));
+
 export const menuItems = pgTable("menu_items", {
   id: serial("id").primaryKey(),
   title: text("title"),
@@ -30,15 +43,37 @@ export const menuItems = pgTable("menu_items", {
   price: real("price"),
   description: text("description"),
   isActive: boolean("is_active"),
+  menu: integer("menu").references(() => menus.id),
+  section: integer("section").references(() => sections.id),
 });
+
+export const menuItemRelations = relations(menuItems, ({ one, many }) => ({
+  menu: one(menus, {
+    fields: [menuItems.menu],
+    references: [menus.id],
+  }),
+  section: one(sections, {
+    fields: [menuItems.section],
+    references: [sections.id],
+  }),
+  ingredients: many(ingredients),
+}));
 
 export const sections = pgTable("sections", {
   id: serial("id").primaryKey(),
   name: text("name"),
 });
 
+export const sectionRelations = relations(sections, ({ many }) => ({
+  menuItems: many(menuItems),
+}));
+
 export const ingredients = pgTable("ingredients", {
   id: serial("id").primaryKey(),
   name: text("name"),
   image: text("image"),
 });
+
+export const ingredientRelations = relations(ingredients, ({ many }) => ({
+  menuItems: many(menuItems),
+}));
