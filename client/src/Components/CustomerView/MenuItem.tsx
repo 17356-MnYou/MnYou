13d4227ca5384ca9189b042e418bf2bb42cd9466
emiller2 <emiller2@andrew.cmu.edu>
2024-04-15 15:27:47 -0400

    Added navigation to item description on click

--- client/src/Components/CustomerView/MenuItem.tsx
+++ client/src/Components/CustomerView/MenuItem.tsx
@@ -1,5 +1,7 @@
 import './MenuItem.css'
 import friedRice from './friedRice.jpeg'
+import { useNavigate } from "react-router-dom";
+
 
 interface iMenuItem { 
   "key": number;
@@ -11,8 +13,17 @@ interface iMenuItem {
 }
 
 function MenuItem(props: iMenuItem) {
+
+  const navigate = useNavigate();
+
+
+  function navigateToItemDescription(){ 
+    console.log("navigatetoitemdescription")
+    navigate(`/menuItem/${props.name}`);
+  }
+
   return (
-    <div className="menuItemBox">
+    <div className="menuItemBox" onClick={navigateToItemDescription}>
     <div className="menuItemImage"><img className="foodImage" src={friedRice}></img></div>
     <div className="menuItemDescription">
       {props.name.length > 14 ? <p><b>{props.name.substring(0,14)}...</b></p> : <p><b>{props.name}</b></p>}
