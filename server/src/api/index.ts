a1765c8a81ab1f763ad4c7a47d80207cf38978f4
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2024-04-16 11:48:47 -0400

    Added / to endpoint

--- server/src/api/index.ts
+++ server/src/api/index.ts
@@ -2,6 +2,6 @@ import express from "express";
 import restaurantsRouter from "./restaurants";
 
 const router = express.Router();
-router.use("restaurants", restaurantsRouter);
+router.use("/restaurants", restaurantsRouter);
 
 export default router;
