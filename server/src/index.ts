5ae68b0fc93273ba7f2cf90a77f48f2b5be63cce
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2024-04-15 21:13:38 -0400

    Created basic endpoints

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,13 @@
 import express from "express";
 import cors from "cors";
+import apiRouter from "./api";
 
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+app.use("/api", apiRouter);
 
 app.listen(process.env.SERVER_PORT, () =>
   console.log(`Server is alive on http://localhost:${process.env.SERVER_PORT}`),
