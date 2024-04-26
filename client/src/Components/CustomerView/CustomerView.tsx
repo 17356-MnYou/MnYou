0fea2ee846adf5389b1c12339733276ed3ac2193
emiller2 <emiller2@andrew.cmu.edu>
2024-04-26 16:22:31 -0400

    added search logic back

--- client/src/Components/CustomerView/CustomerView.tsx
+++ client/src/Components/CustomerView/CustomerView.tsx
@@ -10,15 +10,34 @@ function CustomerView() {
 
   const [menuData, setMenuData] = useState<any[]>([]);
 
+  const [filteredMenuItems, setFilteredMenuItems] = useState<any[]>([]); 
+
   const [storeName, setStoreName] = useState(""); 
 
   const [storeAddress, setStoreAddress] = useState(""); 
+
+  const [storeStyle, setStoreStyle] = useState({
+    primaryFont: '',
+    secondaryFont: '',
+    primaryFontColor: '',
+    secondaryFontColor: '',
+    backgroundColor: ''
+  });
 
   const [open, setOpen] = useState(false); 
 
   const [filters, setFilters] = useState([]);
 
   const dietaryPreferences = ['vegan', 'vegetarian', 'pescetarian', 'dairy free', 'gluten free']
+
+  const dietaryDetails = [{'vegan': ['meat', 'chicken', 'beef', 'fish', 'cheese']}, 
+                          {'vegetarian': ['meat', 'chicken', 'beef', 'fish']},
+                          {'pescetarian': ['fish']},
+                          {'dairy free': ['cheese', 'milk', 'yogurt']}
+  ]
+
+  const ingredientPreferences = ['no olives', 'no broccoli', 'no eggs', 'no cabbage', 'no peppers']
+
 
   function handleClose() { 
     setOpen(false);
@@ -35,6 +54,15 @@ function CustomerView() {
             setMenuData(data.organizedItems);
             setStoreName(data.name); 
             setStoreAddress(data.address);
+            setStoreStyle({
+              primaryFont: data.primaryFont,
+              secondaryFont: data.secondaryFont,
+              primaryFontColor: data.primaryFontColor,
+              secondaryFontColor: data.secondaryFontColor,
+              backgroundColor: data.backgroundColor
+            });
+            console.log("RESPONSEL ", data);
+            setFilteredMenuItems(data.organizedItems);
          })
          .catch((err) => {
             console.log(err.message);
@@ -43,21 +71,37 @@ function CustomerView() {
 
   const [search, setSearch] = useState('');
 
+  useEffect(() => {
+    const filtered = menuData.map(section => ({
+      ...section,
+      items: section.items.filter((item: { title: string; }) => item.title?.toLowerCase().includes(search.toLowerCase()))
+    })).filter(section => section.items.length > 0);
+    console.log("Filtered: ", filtered);
+    setFilteredMenuItems(filtered);
+  }, [search, menuData]);  
+
+
   return (
     <div>
     <Dialog onClose={handleClose} open={open}>
       <DialogTitle>Select filters</DialogTitle>
+      <p style={{marginLeft: 0}}>Dietary restrictions:</p>
       <div className="buttonContainer">
       {dietaryPreferences.map((option, index) => { 
-        return <FilterButton key="index" name={option}/>
+        return <FilterButton style={storeStyle} filters={filters} setFilters={setFilters} key={index} name={option}/>
       })}
       </div>
-      <button>Set filter</button>
+      <p style={{marginLeft: 0}}>Ingredient restrictions:</p>
+      <div className="buttonContainer">
+      {ingredientPreferences.map((option, index) => { 
+        return <FilterButton style={storeStyle} filters={filters} setFilters={setFilters} key={index} name={option}/>
+      })}
+      </div>
     </Dialog>
-    <div>
+    <div style={{ textAlign: 'left',fontFamily: storeStyle.primaryFont, backgroundColor: storeStyle.backgroundColor, color: storeStyle.primaryFontColor}}>
       <h1>{storeName}</h1>
       <p>{storeAddress}</p>
-      <div className="line-separator"></div>
+      <div style={{ background: storeStyle.secondaryFontColor }} className="line-separator"></div>
       <input
         className="searchBar"
         type="text"
@@ -66,10 +110,12 @@ function CustomerView() {
         onChange={e => setSearch(e.target.value)}
       />
       <div className="filterContainer">
-        <button className="buttonFilled">Vegan</button>
+        {filters.map((filterName) => { 
+          return <button style={{ background: storeStyle.secondaryFontColor }}>{filterName}</button>
+        })}
         <button onClick={handleOpen}>+ Filter</button>
       </div>
-      {menuData ? menuData.map((item: any) => (
+      {filteredMenuItems ? filteredMenuItems.map((item: any) => (
         <Section key={item.section_name} sectionTitle={item.section_name} items={item.items} />
       )) : <p>Loading menu...</p>}
     </div>
--- client/src/Components/CustomerView/FilterButton.tsx
+++ client/src/Components/CustomerView/FilterButton.tsx
@@ -3,23 +3,37 @@ import './FilterButton.css';
 
 interface iFilterButton { 
     name: string
+    filters: string[]
+    setFilters: Function
+    style: {
+        primaryFont: string; secondaryFont: string; primaryFontColor: string; secondaryFontColor: string; backgroundColor: string;
+    }
 }
+
 
 function FilterButton(props: iFilterButton) {
     // State to track if the button should have the special class
-    const [isActive, setIsActive] = useState(false);
+    const [isActive, setIsActive] = useState(props.filters.includes(props.name));
 
     // Function to toggle the state
     function toggleClassName() {
         console.log("set className")
         setIsActive(!isActive);
+        if(!isActive){
+            let arr = [...props.filters]; 
+            arr.push(props.name); 
+            props.setFilters(arr);
+        } else { 
+            props.setFilters(props.filters.filter(f => f !== props.name));
+        }
     }
 
     // Conditional class application
     const buttonClass = isActive ? 'buttonFilled' : '';
+    const styleChoice = isActive ? props.style.secondaryFontColor : '';
 
     return (
-        <button className={buttonClass} onClick={toggleClassName}>{props.name}</button>
+        <button style={{background: styleChoice}} className={buttonClass} onClick={toggleClassName}>{props.name}</button>
     );
 }
 
